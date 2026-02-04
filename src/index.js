63de4ea986d5c2856fbe7986bc7596033bca1a71
atharvandure <atharvandure812@gmail.com>
2026-02-04 20:00:14 +0530

    default Tab

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-async function load(filename,event){
-            if(!event && !filename){
-                return;
-            }else{
-                const div=document.getElementsByClassName('left-items')[0];
-                const file=await fetch(filename);
-                console.log("file="+file);
-                const text=await file.text();
-                console.log("text"+text);
-                div.innerHTML=text;
-            }
-
-
+async function load(filename, event) {
+  if (!event && !filename) {
+    return;
+  } else {
+    const div = document.getElementsByClassName("left-items")[0];
+    const file = await fetch(filename);
+    console.log("file=" + file);
+    const text = await file.text();
+    console.log("text" + text);
+    div.innerHTML = text;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const defaultTab = document.querySelector(".left-container a");
+  load("component/message.html", defaultTab);
+});
