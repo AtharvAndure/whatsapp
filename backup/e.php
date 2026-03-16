86410717e63e72d104251f00cff91daf372ff339
atharvandure <atharvandure812@gmail.com>
2026-03-16 23:45:59 +0530

    Nothing NEW

--- backup/e.php
+++ backup/e.php
@@ -3,7 +3,7 @@ header("Content-Type: application/json");
 require_once __DIR__ . "/../connection/connection.php";
 require_once __DIR__ . "/../vendor/autoload.php";
 
-// Check Wether the method is POST or not!
+// Check Wether the method is POST or NOT!
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     echo json_encode(["status" => "error", "message" => "Method is not POST"]);
     exit;
@@ -14,6 +14,7 @@ if($email==""){
     echo json_encode(["status"=>"error","message"=>"Enter Valid Email"]);
     exit;
 }
+    echo json_encode(["status"=>"success","message"=>"Valid Email"]);
 
 
 ?>
